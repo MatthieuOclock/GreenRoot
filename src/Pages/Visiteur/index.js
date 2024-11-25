32c3960eaa5ec652deabc9ceaf7ed09aaeef702b
BriceMendez <brice.mendez@oclock.school>
2024-11-25 11:22:36 +0100

    test

--- src/Pages/Visiteur/index.js
+++ src/Pages/Visiteur/index.js
@@ -1,18 +1,20 @@
-import React from "react"; 
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
-import Home from "./Main/Home/page.js"; 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Contact from "../Visiteur/Main/Contact/page.js";
+import Home from "../Visiteur/Main/Home/page.js";
 
-function Visiteur(){ 
-    return(
-        <div class="main">
-            <Router>
-                <p>a</p>
-                <Routes>
-                    <Route path="*" element= {<Home />} />
-                </Routes>
-            </Router>
-        </div>
-    )
-}; 
+function Visiteur() {
+  return (
+    <div class="main">
+      <Router>
+        <p>a</p>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
-export default Visiteur; 
+export default Visiteur;
