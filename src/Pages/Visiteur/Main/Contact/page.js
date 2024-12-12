1494ba31bb1acea634db51d43e1a00bd614cb861
BriceMendez <brice.mendez@oclock.school>
2024-12-12 11:22:10 +0100

    add sass

--- src/Pages/Visiteur/Main/Contact/page.js
+++ src/Pages/Visiteur/Main/Contact/page.js
@@ -1,13 +1,38 @@
-// import Campains from "../campains/Campains";
-import Description from "./components/Description/text.js";
-import Button from "./components/ButtonDonation/button.js";
 const Contact = () => {
   return (
     <>
-      <div className="home-container" role="main">
-        <Description />
-        {/* <Campains /> */}
-        <Button />
+      <div className="contact-container">
+        <h1>Contact</h1>
+        <form method="post" className="contact-form">
+          <div className="form-group">
+            <label htmlFor="contactReason">Motif de contact:</label>
+            <select id="contactReason" name="contactReason" required>
+              <option value="" disabled>
+                Sélectionner un motif
+              </option>
+              <option value="partenaire">Je souhaite être partenaire</option>
+              <option value="chefDeProjet">
+                Je souhaite être chef de projet
+              </option>
+              <option value="probleme_connexion">
+                J'ai un problème pour me connecter
+              </option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Nom:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" required />
+          </div>
+          <button className="submit-button">Envoyer</button>
+        </form>
       </div>
     </>
   );
